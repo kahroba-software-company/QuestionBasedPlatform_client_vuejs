d68ec82cfcccb04b6c62f022477309c97fe3205b
real_asgari@yahoo.com <real_asgari@yahoo.com>
2020-05-08 18:30:10 +0430

    lets push join-us page

---
+++ pages/join-us.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Hero />
+    <Join_us_form />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/landing/hero_section.vue'
+import Join_us_form from '~/components/web/join_us_form.vue'
+import Footer from '~/components/landing/footer.vue'
+
+export default {
+  components: {
+    Join_us_form,
+    Footer
+  },
+  auth: false,
+  layout: "landing"
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+
+
+</style>
